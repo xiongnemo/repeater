e0f3d6d47686dfcbfe8a73e3ee520ab772381378
Marshmallow <38759782+xiongnemo@users.noreply.github.com>
2018-09-29 22:48:10 +0800

    Create repeater.c

---
+++ repeater.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+void fudu(void);
+int main(void)
+{
+    double power=100.00000;
+    double addpower;
+    printf("此时一位复读机高调经过\n");
+    while (1)
+    {
+       fudu();
+       power=power-(rand()%100+1)/100;
+       if (power<1)
+       {
+            printf("复读机低电量！要充电吗？（输入0不充电）\n");
+            scanf ("%lf",&addpower);
+            power=power+addpower;
+       }
+        if (power<1)
+            break;
+    }
+    return 0;
+}
+void fudu(void)
+{
+    printf("小火汁，想不到你也是个复读机\n");
+}
